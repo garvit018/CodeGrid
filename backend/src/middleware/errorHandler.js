ed2a5325a3fbe81ad68dd43acc8de5de7719c8ab
garvit018 <garvitgoyal83@gmail.com>
2025-09-03 02:33:20 +0530

    Add error handling middleware to log errors and send internal server error response

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+import { logEvents } from "./loggingEvents";
+
+const errorHandler = (err, req, res, next) => {
+  logEvents(`${err.name}: ${err.message}`, "errorLog.txt");
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+};
+
+export default errorHandler;
